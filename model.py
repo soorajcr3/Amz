30a10564378137eb8efd34cb610fda876ee30a2b
soorajcr3 <147608072+soorajcr3@users.noreply.github.com>
2024-02-25 07:21:02 +0000

    3

--- model.py
+++ model.py
@@ -9,10 +9,11 @@ y = data["Qty"]
 
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
-y = le.fit_transform(y)
 
 from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
+
+y = le.fit_transform(y_test)
 
 from sklearn.ensemble import RandomForestRegressor
 model_rf= RandomForestRegressor(n_estimators=100,max_depth=25).fit(x_train,y_train)
